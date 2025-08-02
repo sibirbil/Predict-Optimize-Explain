cd419282ded86fcec5c37a3af8ba6703a0067314
ekiral <erenmehmetkiral@protonmail.com>
2025-08-02 22:37:30 +0900

    G function written

--- example_script.py
+++ example_script.py
@@ -137,6 +137,36 @@ def train(
             optimizer.step()
 
 
+def G_function_benchmark(
+    model,
+    params              : dict[str, torch.Tensor],
+    x0                  : torch.Tensor,     #of shape (A, F) for A in the number of assets
+    benchmark_return    : float,
+    Sigma,
+    lambda_
+    ):
+    A, F = x0.shape         # A: number of assets in portfolio, F: number of features per asset
+    w = cp.Variable(A)  
+    b = cp.Parameter(A) # predicted returns 
+    objective_fn = w.T @ b - (lambda_ / 2) * cp.quad_form(w, Sigma)
+    objective = cp.Maximize(objective_fn)
+    constraints = [
+        cp.sum(w) == 1,  
+        w >= 0           
+    ]
+    problem = cp.Problem(objective, constraints)
+    assert problem.is_dpp()
+    cvxpylayer = CvxpyLayer(problem, parameters=[b], variables=[w])
+
+    def G(x):
+        returns = func.functional_call(model, params, x)
+        solution, = cvxpylayer(returns)
+        return (solution.dot(returns) - benchmark_return)**2
+    
+    gradG =  func.grad(G)
+    return G, gradG
+
+
 
 # permutation = torch.randperm(X_train.shape[0])
 # for i in range(0, (X_train.shape[0]//BatchSize)*BatchSize, BatchSize):
