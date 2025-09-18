1581d8b7235b47bf56024d4dc27d8cb68763b5a2
ekiral <erenmehmetkiral@protonmail.com>
2025-09-18 17:38:19 +0900

    numpy step function langevin is added

--- end2endportfolio/src/langevin.py
+++ end2endportfolio/src/langevin.py
@@ -237,6 +237,163 @@ def torch_MALA_chain(G, gradG, x0, steps=200, eta=1e-3, clip_to=None, device=Non
         traj.append(x.clone())
     return torch.stack(traj, dim=0)
 
+####################################################
+### numpy implementation for locally const funcs ###
+####################################################
+
+import numpy as np
+from typing import NamedTuple, Callable, Union
+
+Array = np.ndarray
+
+class Hyperparameters(NamedTuple):
+    func            : Callable[[Array], Array]  
+    # func is a scalar valued function to apply MALA to,
+    # with signature will be (n, *x.shape)-> (n,) (i.e. batchable)
+    beta            : float                     # inverse temperature
+    eta             : float                     # initial step size
+    # if grad is not given we will approximate 
+    # the gradient of gaussian smoothed func.
+    grad_func       : Optional[Callable]  = None 
+    reg             : Optional[Callable]  = None
+    # target_rate alters step size so that the MALA acceptance rate is within 
+    # 0.8 and 1.2 of the target_rate. If target_rate==None then constant step size
+    target_rate     : Optional[float] = 0.574    
+    # target_change alters smoothing rate so that MALA changes leaf within 
+    # 0.8 and 1.2 percent of target_change. If None then smoothing is constant
+    target_change   : Optional[float] = 0.1   
+    check_every     : int = 100
+    n_samples       : Optional[int] = 1         # number of samples to take for grad comp
+    sigma           : Optional[float] = 0.1     # sigma of the noise
+    
+
+def convolve_and_diff(
+        x           : Array,
+        n_samples   : int,
+        sigma       : float,
+        func        : Callable,
+        grad_func   : Optional[Callable]
+    ):
+    """
+    Get an estimate for the derivative of E[func], the expectation of func,
+    with respect to the mean of the distribution. The distribution is a 
+    normal distribution with fixed variance sigma^2. This is equal to the
+    correlation of F and gaussian noise itself, i.e. ∫F(x + ε)ε dε, which
+    is estimated by random Gaussian samples, making sure -/+ ε are included
+    as pairs, to increase the stability of the estimation.
+    """
+    x_size = x[0].shape
+    if grad_func is None:
+        noise_base = sigma * np.random.randn(n_samples, *x_size)
+        x_pos = x + noise_base    # (n_samples, *x_size)
+        x_neg = x - noise_base    # (n_samples, *x_size)
+
+        T_pos = func(x_pos)
+        T_neg = func(x_neg)
+        # Below approximates the gradient of ∫F(x + ε)ε dε for F = func
+        # where ε ~ N(0,I). Better behaved than ∇F when F is a step function.
+        g = (T_pos - T_neg) @ noise_base/(2*sigma*n_samples)
+        g = g[None,:]
+
+    else:
+        g = grad_func(x)
+
+    return g
+
+def np_MALA_step(
+        x       : Array, 
+        hyps    : Hyperparameters, 
+        eta     : Optional[float] = None,
+        sigma   : Optional[float] = None
+    ):
+
+    if eta is None:
+        eta = hyps.eta/hyps.beta
+    if sigma is None:
+        sigma = hyps.sigma
+    
+    g = convolve_and_diff(x, hyps.n_samples, sigma, hyps.func, hyps.grad_func)
+    g *= hyps.beta
+
+    # Generate noise with same shape as x
+    xi = np.random.randn(*x.shape)
+    
+    # Apply Langevin update: x - eta*g + sqrt(2*eta)*xi
+    update = -eta*g + np.sqrt(2*eta)*xi
+    x_proposed = x + update
+    # Acceptance probability assuming ∇F(x) = ∇F(x') which may be the case 
+    # when ∇F is the gradient of the gaussian convolution of a step function, 
+    # since the convolution is flat within leaves or mostly linear in transitions. 
+    # log q(x|x') - log q(x'|x) = (∇F)^T (-η(∇F) + sqrt(2η)ξ) which is computed
+    # by noting that x' = x - η(∇F(x)) + sqrt(2η)ξ and to go back we would need
+    # x = x' - η(∇F(x')) + sqrt(2η)ξ', for a particular ξ'. Which ends up equaling 
+    # ξ' = sqrt(2η)∇F(x) - ξ. Then we can calculate the log transition density as 
+    # q(x|x') = -(1/2)||ξ'||^2. The forward transition log density is similarly 
+    # q(x'|x) = -(1/2)||ξ||^2. Subtracting we get the cancellation and the result.
+    log_acc_ratio = -hyps.beta*hyps.func(x_proposed) + hyps.beta*hyps.func(x) + np.dot(g, update[0]) 
+    if hyps.reg is not None:
+        log_acc_ratio += -hyps.reg(x_proposed) + hyps.reg(x)              
+    acc_prob = np.exp(np.minimum(0., log_acc_ratio))
+    
+    # Generate random uniform value for acceptance decision
+    uniform_sample = np.random.rand()
+    accepted = (uniform_sample < acc_prob).squeeze()
+    
+    # Accept or reject proposal
+    x_next = x_proposed if accepted else x
+    
+    changed = (hyps.func(x_next) != hyps.func(x))
+    changed = changed.squeeze()
+    
+    return x_next, accepted, changed
+
+
+def adaptive_np_MALA(
+    x       : Array,
+    hyps    : Hyperparameters,
+    Nsteps  : int
+    ):
+    acc = 0     # number of accepted steps
+    chg = 0     # number of steps where leaf changed
+    tot = 0     # total steps taken
+    eta = hyps.eta/hyps.beta
+    sigma = hyps.sigma
+    traj = [x.squeeze()]
+    for step in range(Nsteps):
+        x, accepted, changed = np_MALA_step(x, hyps, eta, sigma)
+        acc += accepted     # accepted is boolean and is converted to 0 or 1.
+        chg += changed      # changed is also a boolean
+        tot += 1
+        traj.append(x.squeeze())
+        if step % hyps.check_every == hyps.check_every-1:
+            eta, sigma = adapt_parameters(acc, chg, tot, eta, sigma, hyps)
+            print(f"On step {step} we have η={eta*hyps.beta:.6f} and σ={sigma:.4f}")
+            acc = 0
+            chg = 0
+            tot = 0     #reset the counters
+
+            
+    return x, np.array(traj)
+
+def adapt_parameters(acc, chg, tot, eta, sigma, hyps: Hyperparameters):
+
+    if hyps.target_rate is None:
+        pass        # eta stays fixed
+    elif acc/tot > hyps.target_rate*1.2:
+        eta = min(1.1*eta, 1./hyps.beta)
+    elif acc/tot < hyps.target_rate*0.8:
+        eta = max(0.9*eta, 1e-3/hyps.beta)
+            
+    if hyps.target_change is None:
+        pass        # sigma stays fixed
+    elif chg/tot > hyps.target_change*2:
+        sigma = max(0.9*sigma, 1e-4)   # smaller sigma decreases horizon
+    elif chg/tot < hyps.target_change/2:
+        sigma = min(1.1*sigma, 1.)      # higher sigma increases horizon
+    
+    print(f"In the last {hyps.check_every} steps {acc} accepted, {chg} times leaf changed")
+
+    return eta, sigma
 
 ######################################################
 # Below is an earlier version which only works with  #
